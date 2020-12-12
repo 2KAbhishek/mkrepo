11ad244c7403e8779708cdbf9d39239fdfe12251
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-12-12 13:05:09 +0530

    Add script

---
+++ mkrepo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+repoName=$1
+templName=$2
+
+if [ -z "$templName" ]; then
+    templName=BareMinimum
+fi
+
+mkdir -p "$repoName"
+
+cd "$repoName" || exit 1
+
+git init
+gh repo create "$repoName" -y -d "Welcome to $repoName" --public -p "$templName"
+git pull origin master --allow-unrelated-histories
+git push --set-upstream origin master
+
+sed -i "s/$templName/$repoName/g" README.md
+git commit -a -m "Update Readme"
