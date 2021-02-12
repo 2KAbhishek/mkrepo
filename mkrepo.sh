6d5311953088337770a997adc51cdbd460dad38f
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-02-12 21:13:50 +0530

    Rename master to main

--- mkrepo.sh
+++ mkrepo.sh
@@ -13,8 +13,9 @@ cd "$repoName" || exit 1
 
 git init
 gh repo create "$repoName" -y -d "Welcome to $repoName" --private -p "$templName"
-git pull origin master --allow-unrelated-histories
-git push --set-upstream origin master
+git pull origin main --allow-unrelated-histories
+git push --set-upstream origin main
 
 sed -i "s/$templName/$repoName/g" README.md
 git commit -a -m "Update Readme"
+
